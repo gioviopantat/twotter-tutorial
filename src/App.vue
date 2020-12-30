a98c04b16be2075b5143b7edd002191c1b87941e
gioviopantat <42895939+gioviopantat@users.noreply.github.com>
2020-12-30 13:50:56 +0800

    add paginition

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,25 @@
   <v-app>
     <v-main>
       <HelloWorld/>
+      <pagination/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld';
+import pagination from './components/pagination';
 
 export default {
   name: 'App',
 
   components: {
     HelloWorld,
+    pagination
   },
 
   data: () => ({
-    //
-  }),
+    v0: true,
+  })
 };
 </script>
--- src/components/pagination.vue
+++ src/components/pagination.vue
@@ -1,68 +1,19 @@
 <template>
-  <!-- <div id="app">
-    <v-app id="inspire">
-      <v-bottom-navigation
-        :value="value"
-        color="teal"
-        grow
-      >
-        <v-btn>
-          <span>Recents</span>
-    
-          <v-icon>mdi-history</v-icon>
-        </v-btn>
-    
-        <v-btn>
-          <span>Favorites</span>
-    
-          <v-icon>mdi-heart</v-icon>
-        </v-btn>
-    
-        <v-btn>
-          <span>Nearby</span>
-    
-          <v-icon>mdi-map-marker</v-icon>
-        </v-btn>
-      </v-bottom-navigation>
-    </v-app>
-  </div> -->
-  <div id="app">
-  <v-app id="inspire">
-    <div>
-      <v-checkbox
-        v-model="v0"
-        label="Visible"
-      ></v-checkbox>
-      <v-banner
-        v-model="v0"
-        single-line
-        transition="slide-y-transition"
-      >
-        No Internet connection
-        <template v-slot:actions="{ dismiss }">
-          <v-btn
-            text
-            color="primary"
-            @click="dismiss"
-          >
-            Dismiss
-          </v-btn>
-          <v-btn
-            text
-            color="primary"
-          >
-            Retry
-          </v-btn>
-        </template>
-      </v-banner>
+    <div id="app">
+        <v-app id="inspire">
+            <div class="text-center">
+                <v-pagination
+                 v-model="page"
+                 :length="6"
+                ></v-pagination>
+            </div>
+        </v-app>
     </div>
-  </v-app>
-  </div>
 </template>
 
 <script>
   export default {
-    name: 'HelloWorld',
+    name: 'pagination',
 
     data: () => ({
       ecosystem: [
